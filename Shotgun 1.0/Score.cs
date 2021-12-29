01ff3c59a7f6699c884b0de3ee01f1ee4ddaa7bf
OzAlbayrak <oz@albayrak.dev>
2021-12-29 12:05:12 +0100

    Added frame for new Score class

---
+++ Shotgun 1.0/Score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shotgun_1._0
+{
+  public class Score
+  {
+    static Score instance;
+    protected Score()
+    {
+      
+    } 
+
+
+
+  }
+}
