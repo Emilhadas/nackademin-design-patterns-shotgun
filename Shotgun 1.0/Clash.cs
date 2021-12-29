18525c5c96cbfc509271acb563eb19357b88311a
Emil Hadås <emilhadas@gmail.com>
2021-12-29 11:32:11 +0100

    Revert "test"
    This reverts commit 5948402a785ec8d3e9bb219c004f02d7c7e8fde0.

---
+++ Shotgun 1.0/Clash.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shotgun_1._0
+{
+    class Clash
+    {
+        public static int pA;
+        public static int cA;
+        public static string  Result;
+
+        public string Fight(int choice)
+        {
+
+
+            PlayerActions pa = new PlayerActions();
+            CpuActions ca = new CpuActions();
+
+
+            pA = pa.PlayerChoice(choice);
+            cA = ca.Action();
+
+
+
+            if (pA == 1 && cA != 3 && cA != 4)
+            {
+                Result = "Reloaded Successfully";
+                return Result;
+
+            }
+
+            else if (pA == 1 && cA == 3 || (cA == 4 && pA != 4))
+            {
+                Result = "You Lose";
+                return Result;
+
+            }
+
+            else if (pA == 2 && cA == 3)
+            {
+                Result = "You Block a Shot";
+                return Result;
+
+            }
+            else if (pA == 2 && cA == 4)
+            {
+                Result = "You Lose";
+                return Result;
+
+            }
+
+
+            else if (pA == 3 && cA == 1)
+            {
+                Result = "You Win!";
+                return Result;
+
+            }
+            else if (pA == 3 && cA == 2)
+            {
+                Result = "You were Blocked";
+                return Result;
+
+            }
+            else if (pA == 3 && cA == 4)
+            {
+                Result = "You Lose";
+                return Result;
+
+            }
+            else if (pA == 2 && cA == 1)
+            {
+                Result = "You blocked air, enemy reloaded successfully";
+                return Result;
+            }
+            else if (pA == 4 && cA != 4)
+            {
+                Result = "You Win!";
+                return Result;
+            }
+            else
+            {
+                Result = "You both countered eachother";
+                return Result;
+
+            }   
+        }
+    }
+}
---
+++ Shotgun 1.0/PlayerActions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shotgun_1._0
+{
+    class PlayerActions
+    {
+        public static int PlayerAmmo;
+
+        public  int PlayerChoice(int choice)
+        {
+
+            int choiceToCounterWith = choice;
+
+            if (choice == 1)
+            {
+                PlayerAmmo++;
+
+            }
+
+            else if (choice == 3)
+            {
+                PlayerAmmo--;
+            }
+
+            else if (choice == 4)
+            {
+                PlayerAmmo -= 3;
+            }
+
+            return choiceToCounterWith;
+        }
+
+        public int PlayerAmmunition()
+        {
+            return PlayerAmmo;
+
+        }
+
+
+        public void ScoreReset()
+        {
+            PlayerAmmo = 0;
+        }
+    }
+}
